7d10ef37557d8706667db58eb007a5b0f22e232b
Óscar Quinto Coll <oscarcollquinto@gmail.com>
2018-02-26 18:31:27 +0100

    Versión Junit sin fallos

--- DepositoCombustible/src/DepositoCombustibleTest.java
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -5,38 +5,40 @@ import org.junit.Test;
 
 public class DepositoCombustibleTest {
 
-	DepositoCombustible tank = new DepositoCombustible(40.0,0.0);
+	DepositoCombustible tank = new DepositoCombustible(40.0,00.0);
 	
 	//1
 	@Test
-	(expected=Exception.class)
 	public final void testGetDepositoNivel() throws Exception{
 		assertEquals(0.0,tank.getDepositoNivel(),0);
 	}
 	
 	//2
 	@Test
-	(expected=Exception.class)
 	public final void testGetDepositoMax() throws Exception{
 		assertEquals(40.0, tank.getDepositoMax(),0);
 	}
 
 	//3
 	@Test
-	(expected=Exception.class)
 	public final void testEstaVacio() throws Exception{
 		assertTrue(tank.estaVacio());
 	}
 	
 	//4
 	@Test
-	public final void testEstaLleno() {
-		assertTrue(tank.estaLleno());;
+	public final void testEstaLleno() throws Exception{
+		tank.fill(20.0);
+		
+		double a = tank.getDepositoMax();
+		double b = tank.getDepositoNivel();
+		double c = a/2;
+		
+		assertEquals(c, b, 0);
 	}
 
 	//5
 	@Test
-	(expected=Exception.class)
 	public final void testConsumir() throws Exception{
 		tank.fill(100);
 		tank.consumir(50);
@@ -45,7 +47,6 @@ public class DepositoCombustibleTest {
 
 	//6
 	@Test
-	(expected=Exception.class)
 	public final void testFill() throws Exception{
 		tank.fill(100);
 		assertEquals(100.0, tank.getDepositoNivel(), 0);
